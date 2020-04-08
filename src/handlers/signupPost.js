89c36b2498e1c17bbfa6e4f9742367c4440d6452
tacotoemeck <wagner.tomek@gmail.com>
2020-04-08 14:53:51 +0100

    Add new routes

---
+++ src/handlers/signupPost.js
@@ -0,0 +1,7 @@
+const templates = require("../template");
+const db = require("../db/connection.js");
+const model = require("../model.js");
+
+function signupHandler() {}
+
+module.exports = signHandler;
