f4798e64036519e29a52457ed441889b613d87cd
Chloe <chloeh24@hotmail.com>
2020-04-09 19:00:01 +0100

    change css styling

--- src/template.js
+++ src/template.js
@@ -5,7 +5,7 @@ function sharedLayout(bodyContent, navButton) {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta name="description" content="Tiger Kingdom">
-            <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Lato:wght@300&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Gotu&family=Lato:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
             <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
 
             <link rel="stylesheet" href="./public/main.css">
