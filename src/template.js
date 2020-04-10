b5c1effcdadeee4a4251c89268f7c56a9d1d4fb2
Chloe <chloeh24@hotmail.com>
2020-04-10 11:36:52 +0100

    make passwords hidden

--- src/template.js
+++ src/template.js
@@ -119,11 +119,11 @@ function signupPage() {
           class="signup_inputs"
           id="signup_password"
           name="signup_password"
+          type='password'
           placeholder="Don't do password123!"
           required
           aria-describedby="passwordRequirements passwordError"
           required
-          pattern="(?=.*[A-z])(?=.*\d)[A-z\d]+"
           minlength="8"
         />
 
@@ -145,6 +145,7 @@ function loginPage() {
         <input
           id="login_username"
           name="login_username"
+          type='password'
           placeholder="Username please"
           required
         />
